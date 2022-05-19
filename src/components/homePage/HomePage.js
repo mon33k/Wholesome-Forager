271e6e82c2cdec360c8666fff4daeb05b71b99a7
mon33k <moniquemojica@pursuit.org>
2022-05-19 13:23:19 -0400

    added more styling

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -14,16 +14,16 @@ const HomePage = () => {
           <p>New York State</p>
         </div>
         <img src={firstImage} className="first-image" />
-      </div>
-      <div>
-        <h3>Information sourced from USDA and PFAF</h3>
-        <div className="resources-box">
-          <h4>
-            For more information please refer to these sources:<br></br>
-            <a href="https://plants.sc.egov.usda.gov/home">USDA Database</a>
-            <br></br>
-            <a href="https://pfaf.org/user/Default.aspx">PFAF Database</a>
-          </h4>
+        <div className="disclaimer-nested-container">
+          <h3>Information sourced from USDA and PFAF</h3>
+          <div className="resources-box">
+            <h4>
+              For more information please refer to these sources:<br></br>
+              <a href="https://plants.sc.egov.usda.gov/home">USDA Database</a>
+              <br></br>
+              <a href="https://pfaf.org/user/Default.aspx">PFAF Database</a>
+            </h4>
+          </div>
         </div>
       </div>
       <div>
@@ -31,11 +31,16 @@ const HomePage = () => {
           <img
             src="https://loveincorporated.blob.core.windows.net/contentimages/main/foraging+.jpg"
             alt="plants"
+            className="second-image"
           />
         </div>
 
-        <div>
-          <img src={iphoneexample} alt="iphoneexample" />
+        <div className="third-img-container">
+          <img
+            src={iphoneexample}
+            alt="iphoneexample"
+            className="third-image"
+          />
         </div>
       </div>
     </>
