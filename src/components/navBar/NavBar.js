bc6701f283b9e962e6ed9677edb5df28952cac0f
mon33k <moniquemojica@pursuit.org>
2022-05-23 22:18:08 -0400

    added nav and logo

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -3,7 +3,9 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./navBar.css";
 import PlantList from "../plantList/PlantList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faPersonHiking } from "@fortawesome/free-solid-svg-icons";
+// import { fab } from "@fortawesome/free-brands-svg-icons";
+
 // import PlantPage from "../plantPage/PlantPage";
 import HomePage from "../homePage/HomePage";
 import PlantPage from "../plantPage/PlantPage";
@@ -19,14 +21,17 @@ const NavBar = () => {
   return (
     <>
       <div className="nav-container">
-        <Link to="/">Wholesome Forager</Link>
+        <Link to="/" className="logo-icon">
+          Wholesome Forager
+          <FontAwesomeIcon icon={faPersonHiking} className="hiking-icon" />
+        </Link>
         <div className="burger-menu">
           <FontAwesomeIcon icon={faBars} onClick={handleMenuClick} />
           {clicked ? (
             <div className="menu-overlay">
               <Link to="/plant/list">Plant Archive</Link>
               <Link to="/favorite">Favorite Plants</Link>
-              <Link to="/search">Search</Link>
+              <SearchPlant />
             </div>
           ) : (
             ""
@@ -38,7 +43,7 @@ const NavBar = () => {
         <Route path="/plant/list" element={<PlantList />} />
         <Route path="/plant/:id" element={<PlantPage />} />
         <Route path="/favorite" element={<FavoritePlants />} />
-        <Route path="/search" element={<SearchPlant />} />
+        <Route path="/search/:text" element={<SearchPlant />} />
       </Routes>
     </>
   );
